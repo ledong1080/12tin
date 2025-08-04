b79733c72a98c53d549509660eaacbc6c91e63f5
ledong1080 <67459708+ledong1080@users.noreply.github.com>
2025-08-04 21:55:21 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/12tin/', // Thêm dòng này để build đúng cho GitHub Pages
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
